35ef87a76fbabb0ec57239fe522828eb3f5fde56
Humayra Ferdous <37269881+HumayraFerdous@users.noreply.github.com>
2020-11-08 12:29:36 +0600

    Base file

---
+++ base.h
@@ -0,0 +1,46 @@
+#ifndef BASE_H_INCLUDED
+#define BASE_H_INCLUDED
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+using namespace std;
+
+class base
+{
+protected:
+    char name[100];
+    char offday[100];
+    char destin[100];
+public:
+    base()
+    {
+        strcpy(name," ");
+        strcpy(offday," ");
+        strcpy(destin," ");
+    }
+    base(char s[],char p[],char t[])
+    {
+        strcpy(name,s);
+        strcpy(offday,p);
+        strcpy(destin,t);
+    }
+    void getinfo()
+    {
+        cout<<"\tTrain name: ";
+        cin>>name;
+        cout<<"\tOffday : ";
+        cin>>offday;
+        cout<<"\tDestination: ";
+        cin>>destin;
+    }
+    void displayinfo()
+    {
+        cout<<"\tName "<<setw(15)<<":"<<name<<endl;
+        cout<<"\tOffday is "<<setw(10)<<":"<<offday<<endl;
+        cout<<"\tDestination "<<setw(8)<<":"<<destin<<endl;
+    }
+    virtual void input()= 0;
+    virtual void output()= 0;
+};
+
+#endif // BASE_H_INCLUDED
